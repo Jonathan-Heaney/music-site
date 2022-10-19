a3bc42eb5e21e0db3f2d79d8c9a589c7a4ce2dc7
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-10-19 12:15:48 -0500

    Add bio page

--- src/bio.js
+++ src/bio.js
@@ -1,3 +1,16 @@
 'use strict';
 
-function createBioPage() {}
+export const createBio = createBioPage();
+
+function createBioPage() {
+  const content = document.getElementById('content');
+  const main = document.createElement('main');
+  content.appendChild(main);
+  const header = document.createElement('h1');
+  header.textContent = 'Biography';
+  main.appendChild(header);
+  const paragraph = document.createElement('p');
+  paragraph.textContent =
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque nostrum, iure fugiat a expedita dolorem illum quaerat ut at dolore repudiandae similique, illo consectetur esse perferendis animi natus voluptate. Animi aliquid, reprehenderit aliquam repudiandae saepe soluta eum delectus voluptas, porro excepturi odio explicabo iusto? Vitae ut fugit repellendus, aperiam magnam voluptatum quia voluptate impedit commodi iusto aspernatur asperiores odit.';
+  main.appendChild(paragraph);
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@
 import './main.css';
 import { createNav } from './nav';
 import { createHome } from './home';
-
-console.log('Hello');
+// import { createBio } from './bio';
 
 createNav;
 createHome;
+
+const home = document.getElementById('home');
+const bio = document.getElementById('bio');
+const media = document.getElementById('media');
+const calendar = document.getElementById('calendar');
+const contact = document.getElementById('contact');
+
+home.addEventListener('click', createHome);
+// bio.addEventListener('click', createBio);
